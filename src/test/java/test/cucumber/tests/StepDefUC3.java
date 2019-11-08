89e0e885bad27dfe8a2373cc3064c8d6dac3846c
katherineyecc <katherineyecc@163.com>
2019-11-08 08:20:35 -0500

    test for UC3's incorrect path of term ending.

--- src/test/java/test/cucumber/tests/StepDefUC3.java
+++ src/test/java/test/cucumber/tests/StepDefUC3.java
@@ -23,6 +23,7 @@ public class StepDefUC3 extends TestCase {
 	boolean success = false;
 	String identity = "clerk";
 	String password = "admin1";
+	
 	@When("^The Clerk logs into ATS and enters \"([^\"]*)\" and enters \"([^\"]*)\"$")
 	public void the_Clerk_logs_into_ATS_and_enters_and_enters(String arg1, String arg2) {
 		ats = new ATServer(Config.DEFAULT_PORT);
@@ -83,5 +84,70 @@ public class StepDefUC3 extends TestCase {
 	@Then("^The course is created successfully$")
 	public void the_course_is_created_successfully() {
 	    assertEquals(true, success);
+	}
+	
+	@When("^The Clerk logs into ATS and enters \"([^\"]*)\" and \"([^\"]*)\" and current term ends$")
+	public void the_Clerk_logs_into_ATS_and_enters_and_and_current_term_ends(String arg1, String arg2) throws AWTException {
+		ats = new ATServer(Config.DEFAULT_PORT);
+		Thread t = new Thread(ats);
+		t.start();
+		try {
+			robot = new Robot();
+			robot.delay(8000);
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(500);
+			for(int index=0; index<identity.length(); index++) {
+				char c = identity.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(1000);
+			for(int index=0; index<password.length(); index++) {
+				char c = password.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(1000);
+			
+			robot.delay(20*118);//wait for this term to end
+			
+			for(int index=0; index<arg1.length(); index++) {
+				char c = arg1.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(1000);
+			for(int index=0; index<arg2.length(); index++) {
+				char c = arg2.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(1000);
+		} catch(AWTException e) {
+			e.printStackTrace();
+		}
+		int state = ats.getClientState();
+		if(state == 2 && Config.TERM_ENDS == true)
+			success = true;
+		ats = null;
+		robot = null;
+	}
+
+	@Then("^The Clerk cannot create the courses$")
+	public void the_Clerk_cannot_create_the_courses() throws AWTException {
+		assertEquals(true, success);
 	}
 }
---
+++ src/test/java/test/cucumber/tests/UC3_2.feature
@@ -0,0 +1,12 @@
+@tag
+Feature: Title of your feature
+  I want to use this template for my feature file
+
+  @tag1
+  Scenario: The Clerk fails to create a course due to term ending
+  Given The ATS is on
+  
+  When The Clerk logs into ATS and enters "create course" and "OO Dev, 105104, 30, n, 0, 4, n, n" and current term ends
+  
+  Then The Clerk cannot create the courses
+    
