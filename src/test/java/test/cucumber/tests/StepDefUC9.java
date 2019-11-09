9f06762e19b44f6c59a0613777a6e1de9cc0add4
katherineyecc <katherineyecc@163.com>
2019-11-09 15:49:40 -0500

    test for UC9's correct path.

---
+++ src/test/java/test/cucumber/tests/StepDefUC9.java
@@ -0,0 +1,74 @@
+package test.cucumber.tests;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import junit.framework.TestCase;
+import main.server.network.ATServer;
+import main.utilities.Config;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class StepDefUC9 extends TestCase {
+
+	ATServer ats;
+	Robot robot;
+	boolean success = false;
+	String identity = "student";
+	String stuInfo = "101075401, tom";
+	@When("^The Student logs into the system and enters \"([^\"]*)\"$")
+	public void the_Student_logs_into_the_system_and_enters(String arg1) throws Throwable {
+		ats = new ATServer(Config.DEFAULT_PORT);
+		Thread t = new Thread(ats);
+		t.start();
+		try {
+			robot = new Robot();
+			robot.delay(8000);
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<identity.length(); index++) {
+				char c = identity.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<stuInfo.length(); index++) {
+				char c = stuInfo.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<arg1.length(); index++) {
+				char c = arg1.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+		} catch(AWTException e) {
+			e.printStackTrace();
+		}
+		int state = ats.getClientState();
+		String output = ats.output;
+		if(state == 0) {
+			if(output.contentEquals("Successfully Log Out!\n")) {
+				success = true;
+			}
+		}
+		ats = null;
+		robot = null;
+	}
+
+	@Then("^The Student logs out the system successfully$")
+	public void the_Student_logs_out_the_system_successfully() throws Throwable {
+		assertEquals(true, success);
+	}
+}
---
+++ src/test/java/test/cucumber/tests/UC9_1.feature
@@ -0,0 +1,12 @@
+@tag
+Feature: Title of your feature
+  I want to use this template for my feature file
+
+  @tag1
+  Scenario: Student logs out the ATS successfully
+  Given The ATS is on
+  
+  When The Student logs into the system and enters "log out"
+  
+  Then The Student logs out the system successfully
+  
