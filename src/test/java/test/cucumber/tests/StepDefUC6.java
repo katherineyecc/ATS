eec5abb17fe3db12c96057b2450492216ff57a83
katherineyecc <katherineyecc@163.com>
2019-11-09 14:34:13 -0500

    test for UC6's incorrect path of registration starting.

--- src/test/java/test/cucumber/tests/StepDefUC6.java
+++ src/test/java/test/cucumber/tests/StepDefUC6.java
@@ -147,5 +147,66 @@ public class StepDefUC6 extends TestCase {
 	public void the_Clerk_cannot_create_the_student() throws Throwable {
 		assertEquals(true, success);
 	}
+	
+	@When("^The Clerk logs into ATS and enters \"([^\"]*)\" and enters info \"([^\"]*)\" and current term does not end and registration starts$")
+	public void the_Clerk_logs_into_ATS_and_enters_and_enters_info_and_current_term_does_not_end_and_registration_starts(String arg1, String arg2) throws Throwable {
+		ats = new ATServer(Config.DEFAULT_PORT);
+		Thread t = new Thread(ats);
+		t.start();
+		try {
+			robot = new Robot();
+			robot.delay(8000);
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<identity.length(); index++) {
+				char c = identity.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<password.length(); index++) {
+				char c = password.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<arg1.length(); index++) {
+				char c = arg1.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<arg2.length(); index++) {
+				char c = arg2.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+		} catch(AWTException e) {
+			e.printStackTrace();
+		}
+		int state = ats.getClientState();
+		String output = ats.output;
+		if(state == 2) {
+			if(output.contentEquals("Overdue!\n")) {
+				success = true;
+			}
+		}
+		ats = null;
+		robot = null;
+	}
 
 }
