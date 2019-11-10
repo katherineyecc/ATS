15d77594e7f70e199e9ab50fc6e579ceeee94f80
katherineyecc <katherineyecc@163.com>
2019-11-09 21:51:51 -0500

    test for UC11's incorrect path of term ending.

--- src/test/java/test/cucumber/tests/StepDefUC11.java
+++ src/test/java/test/cucumber/tests/StepDefUC11.java
@@ -72,4 +72,108 @@ public class StepDefUC11 extends TestCase {
 	public void the_Student_cannot_deregister_for_a_course() throws Throwable {
 		assertEquals(true, success);
 	}
+	
+	@When("^The Student logs in ATS and inputs \"([^\"]*)\" and \"([^\"]*)\" and \"([^\"]*)\" and \"([^\"]*)\" and \"([^\"]*)\" and \"([^\"]*)\" and term ends$")
+	public void the_Student_logs_in_ATS_and_inputs_and_and_and_and_and_and_term_ends(String arg1, String arg2, String arg3, String arg4, String arg5, String arg6) throws Throwable {
+		ats = new ATServer(Config.DEFAULT_PORT);
+		Thread t = new Thread(ats);
+		t.start();
+		try {
+			robot = new Robot();
+			robot.delay(5000);
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<identity.length(); index++) {
+				char c = identity.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<stuInfo.length(); index++) {
+				char c = stuInfo.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<arg1.length(); index++) {
+				char c = arg1.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<arg2.length(); index++) {
+				char c = arg2.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<arg3.length(); index++) {
+				char c = arg3.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<arg4.length(); index++) {
+				char c = arg4.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			
+			robot.delay(60000);//delay for term to end
+			robot.delay(30000);
+			//registration starts at 20s
+			//registration ends at 34s
+			//term ends at 118s
+			
+			for(int index=0; index<arg5.length(); index++) {
+				char c = arg5.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+			for(int index=0; index<arg6.length(); index++) {
+				char c = arg6.charAt(index);
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+				robot.delay(200);
+			}
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+			robot.delay(200);
+		} catch(AWTException e) {
+			e.printStackTrace();
+		}
+		int state = ats.getClientState();
+		String output = ats.output;
+		if(state == 4) {
+			if(output.contentEquals("Term ends!\n")) {
+				success = true;
+			}
+		}
+		ats = null;
+		robot = null;
+	}
 }
